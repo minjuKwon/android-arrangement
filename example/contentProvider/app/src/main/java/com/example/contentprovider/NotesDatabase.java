5eec9f4548be47afa05c51da886bccd3da8b43ad
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-12-15 15:06:17 +0900

    Update NotesDatabase for add kotlin table

--- example/contentProvider/app/src/main/java/com/example/contentprovider/NotesDatabase.java
+++ example/contentProvider/app/src/main/java/com/example/contentprovider/NotesDatabase.java
@@ -10,8 +10,14 @@ public class NotesDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_NAME="notes.db";
     private static final int DATABASE_VERSION=1;
 
-    public static final class NotesEntry implements BaseColumns{
-        static final String TABLE_NAME="notes";
+    public static final class JavaNotesEntry implements BaseColumns{
+        static final String TABLE_NAME="java_notes";
+        static final String COLUMN_TITLE="title";
+        static final String COLUMN_CONTENT="content";
+    }
+
+    public static final class KotlinNotesEntry implements BaseColumns{
+        static final String TABLE_NAME="kotlin_notes";
         static final String COLUMN_TITLE="title";
         static final String COLUMN_CONTENT="content";
     }
@@ -22,17 +28,26 @@ public class NotesDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String SQLITE_CREATE_NOTES_TABLE=
-                "CREATE TABLE "+NotesEntry.TABLE_NAME +"("+
-                NotesEntry._ID +"INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                NotesEntry.COLUMN_TITLE+" TEXT, "+
-                NotesEntry.COLUMN_CONTENT+" TEXT);";
-        db.execSQL(SQLITE_CREATE_NOTES_TABLE);
+        final String SQLITE_CREATE_JAVA_TABLE=
+                "CREATE TABLE "+JavaNotesEntry.TABLE_NAME +"("+
+                JavaNotesEntry._ID +"INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                JavaNotesEntry.COLUMN_TITLE+" TEXT NOT NULL, "+
+                JavaNotesEntry.COLUMN_CONTENT+" TEXT NOT NULL);";
+
+        final String SQLITE_CREATE_KOTLIN_TABLE=
+                "CREATE TABLE "+KotlinNotesEntry.TABLE_NAME +"("+
+                KotlinNotesEntry._ID +"INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                KotlinNotesEntry.COLUMN_TITLE+" TEXT NOT NULL, "+
+                KotlinNotesEntry.COLUMN_CONTENT+" TEXT NOT NULL);";
+
+        db.execSQL(SQLITE_CREATE_JAVA_TABLE);
+        db.execSQL(SQLITE_CREATE_KOTLIN_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS notes");
+        db.execSQL("DROP TABLE IF EXISTS "+JavaNotesEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+KotlinNotesEntry.TABLE_NAME);
         onCreate(db);
     }
 
