c89f9f86b7b7150fb4d857a3bb37e029ccf70293
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-12-21 23:56:14 +0900

    Add click event in MainActivity.java

--- example/contentProvider/app/src/main/java/com/example/contentprovider/MainActivity.java
+++ example/contentProvider/app/src/main/java/com/example/contentprovider/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.contentprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.contentprovider.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity{
+
+    private ActivityMainBinding binding;
+    private NotesRecyclerviewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        adapter=new NotesRecyclerviewAdapter(this);
+        binding.javaList.setLayoutManager(new LinearLayoutManager(this));
+        binding.kotlinList.setLayoutManager(new LinearLayoutManager(this));
+        binding.javaList.setAdapter(adapter);
+        binding.kotlinList.setAdapter(adapter);
+
+        binding.btnDelete.setOnClickListener(v->deleteData());
+        binding.btnInsert.setOnClickListener(v->insertData());
+        binding.btnUpdate.setOnClickListener(v->updateData());
     }
+
+    public void deleteData(){
+
+    }
+
+    public void insertData(){
+
+    }
+
+    public void updateData(){
+
+    }
+
 }
