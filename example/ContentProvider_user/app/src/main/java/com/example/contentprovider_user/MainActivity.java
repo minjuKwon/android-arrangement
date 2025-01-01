6bc178e9720d8dc8d430031afd14fee55923cd20
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-01-01 21:17:15 +0900

    Add click event in MainActivity.java

--- example/ContentProvider_user/app/src/main/java/com/example/contentprovider_user/MainActivity.java
+++ example/ContentProvider_user/app/src/main/java/com/example/contentprovider_user/MainActivity.java
@@ -2,23 +2,28 @@ package com.example.contentprovider_user;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.contentprovider_user.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnDelete.setOnClickListener(v->deleteAllData());
+        binding.btnQuery.setOnClickListener(v->queryData());
     }
+
+    public void deleteAllData(){
+
+    }
+
+    public void queryData(){
+
+    }
+
 }
