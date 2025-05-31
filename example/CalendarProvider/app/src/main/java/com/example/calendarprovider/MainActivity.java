5bcba41b11bfab7bf02a60a87f6b6fae4a0e8731
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-05-31 17:46:31 +0900

    Feat: Call calendar manager

--- example/CalendarProvider/app/src/main/java/com/example/calendarprovider/MainActivity.java
+++ example/CalendarProvider/app/src/main/java/com/example/calendarprovider/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.calendarprovider;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EdgeToEdge.enable(this);
+
+        CalendarManager manager=new CalendarManager(this, 1);
+        Thread thread=new Thread(() -> {
+            //calendar
+            manager.queryCalendar();
+            manager.updateCalendar();
+            manager.queryCalendar();
+
+            //event
+            long eventId=manager.insertEvent();
+            manager.updateEvent(eventId);
+            manager.viewEvent(eventId);
+            manager.viewSpecifiedEvent();
+
+            //attendee
+            long attendeeAId=manager
+                    .insertAttendee(eventId,"A","a@mail.com",
+                            CalendarContract.Attendees.RELATIONSHIP_ATTENDEE,CalendarContract.Attendees.TYPE_OPTIONAL,
+                            CalendarContract.Attendees.ATTENDEE_STATUS_ACCEPTED);
+            manager.insertAttendee(eventId,"b","a@mail.com",
+                    CalendarContract.Attendees.RELATIONSHIP_ATTENDEE,CalendarContract.Attendees.TYPE_OPTIONAL,
+                    CalendarContract.Attendees.ATTENDEE_STATUS_ACCEPTED);
+            manager.updateAttendee(attendeeAId);
+            manager.deleteAttendee(attendeeAId);
+
+            //reminder
+            long reminderId=manager.insertReminder(eventId);
+            manager.updateReminder(reminderId);
+
+            //instance
+            manager.queryInstance(eventId);
         });
+
+        thread.start();
+
     }
+
 }
